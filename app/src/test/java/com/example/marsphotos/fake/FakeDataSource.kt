fe54d8672a4b7c38f891584b559d5eb7defd1c80
Abdallah Ben Salem <abenssalem32@gmail.com>
2024-11-21 14:32:21 +0100

    commit

---
+++ app/src/test/java/com/example/marsphotos/fake/FakeDataSource.kt
@@ -0,0 +1,26 @@
+package com.example.marsphotos.fake
+
+import com.example.marsphotos.model.MarsPhoto
+import com.example.marsphotos.network.MarsApiService
+
+object FakeDataSource  : MarsApiService {
+
+    const val idOne = "img1"
+    const val idTwo = "img2"
+    const val imgOne = "url.1"
+    const val imgTwo = "url.2"
+    val photosList = listOf(
+        MarsPhoto(
+            id = idOne,
+            imgSrc = imgOne
+        ),
+        MarsPhoto(
+            id = idTwo,
+            imgSrc = imgTwo
+        )
+    )
+
+    override suspend fun getPhotos(): List<MarsPhoto> {
+        return FakeDataSource.photosList
+    }
+}
